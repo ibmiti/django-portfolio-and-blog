25f1a0e912eccf8ce30d8cabb8167668374eaf7a
Dimitri M <ibmiti@gmail.com>
2021-09-17 17:45:57 -0400

    created model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# create class, class will represent the table,columns within the db
+# every project will have the below features.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
