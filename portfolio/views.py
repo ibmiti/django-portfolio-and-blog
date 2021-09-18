aac2e4fe6debe9d8153aa3bacdb68f1acf0e1964
Dimitri M <ibmiti@gmail.com>
2021-09-18 15:29:25 -0400

    displaying all items within projects model to the home view

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,6 @@ from .models import Project
 
 def home(request):
     # place all items in db.table.project into projects var.
-    projects = Project.objects().all()
+    projects = Project.objects.all()
     # send collected items ( table data ( column, rows )) to the home view.
     return render(request, 'portfolio/home.html', {'projects':projects})
