bfdaf4259a9f408a225e121b42ab0739d6a0bdf9
Dimitri M <ibmiti@gmail.com>
2021-09-18 09:21:16 -0400

    created a home method which accepts only a request, and then sets all items in db.table.project into project var. lastly, returned + rendered the request, 'path', project_object ( cont. data )

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def home(request):
+    # place all items in db.table.project into projects var.
+    projects = Project.objects().all()
+    # send collected items ( table data ( column, rows )) to the home view.
+    return render(request, 'portfolio/home.html', {'projects':projects})
