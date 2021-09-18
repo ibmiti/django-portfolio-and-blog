44531c5e0aeedf8d1cc40d77012abed8c4e4223d
Dimitri M <ibmiti@gmail.com>
2021-09-18 16:29:22 -0400

    adding notes

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from .models import Blog
 
 def all_blogs(request):
     # give a total of 5 objs and order by date in decending order, ( give most recent articles )
+    # similar to paginating or grouping or chunking the results of the query.
     blogs = Blog.objects.all().order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
